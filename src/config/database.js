3b544e9febd05ce7973e52f1f4c27de3bf5422f9
Eduardo Backes <e.backes2011@gmail.com>
2019-06-25 00:37:53 -0300

    Data validation

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,9 @@
 module.exports = {
   dialect: 'postgres',
   host: 'localhost',
+  port: 5433,
   username: 'postgres',
-  password: 'postgres',
+  password: 'docker',
   database: 'gobarber',
   define: {
     timestamps: true,
