518567994e49e611c8cd0321f6472fc374fde1ec
Eduardo Backes <e.backes2011@gmail.com>
2019-06-26 22:11:00 -0300

    Mail configuration and send email

---
+++ src/config/mail.js
@@ -0,0 +1,19 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '2adcce5f6b1012',
+    pass: '8c89a089e1f95e',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+// Amazon SES
+// Mailgun
+// Sparkhost
+// Mandril (Mailchimp)
+
+// Mailtrap (DEV) - https://mailtrap.io
